fb87dc10f423e3e68308e64c7c8f562ce8f5a858
git@buysdb.nl <git@buysdb.nl>
2022-02-17 12:19:49 +0100

    Started adding some doc

--- pyscicone/scicone/tree.py
+++ pyscicone/scicone/tree.py
@@ -214,9 +214,47 @@ class Tree(object):
         self.tree_str = ''.join(list_tree_file)
         self.read_tree_str(self.tree_str, num_labels=num_labels)
 
-    def learn_tree(self, segmented_data, segmented_region_sizes, n_iters=1000, move_probs=[0.0,1.0,0.0,1.0, 0.0, 1.0, 0.0, 1.0, 0.0, 1.0, 0.01, 0.1, 0.01, 1.0, 0.01],
-                    n_nodes=3,  seed=42, postfix="", initial_tree=None, nu=1.0, cluster_sizes=None, region_neutral_states=None, alpha=0., max_scoring=True, copy_number_limit=2,
-                    c_penalise=10.0, lambda_r=0.2, lambda_c=0.1, ploidy=2, verbosity=1, verbose=False, num_labels=False):
+    def learn_tree(self,
+                   segmented_data: np.array, # counts matrix
+                   segmented_region_sizes: np.array,
+                   n_iters: int=1000,  # Number of iterations
+                   move_probs:  list=[0.0,1.0,0.0,1.0, 0.0, 1.0, 0.0, 1.0, 0.0, 1.0, 0.01, 0.1, 0.01, 1.0, 0.01],
+                   n_nodes=3,  # the number of nodes in the random initialised tree
+
+                   seed=42,
+                   postfix="",
+                   initial_tree=None,
+                   nu=1.0,  #nu parameter, the overdispersion variable
+                   cluster_sizes=None,
+                   region_neutral_states=None,
+                   # gamma parameter, the initial learning rate value is not broken out
+                   alpha=0., # alpha parameter, the constant in the learning rate
+                   max_scoring=True,
+                   copy_number_limit=2,
+                   c_penalise=10.0,  # term that penalises trees containing cancelling events to be added to tree event prior
+                   lambda_r=0.2,  # lambda param for the poisson that generates the number of regions
+                   lambda_c=0.1,  # l 	lambda param for the poisson that generates the copy number state of a region
+                   ploidy=2,
+                   verbosity=1,
+                   verbose=False,
+                   num_labels=False):
+
+        # cf : cluster fraction variable between 0 and 1 to affect the tree prior coefficient
+        #
+        """
+        Finds the maximum a posteriori tree given a cells by regions matrix.
+
+        Args:
+            segmented_data(np.array): Counts matrix
+
+            n_iters(int): Number of iterations
+
+
+
+
+
+        """
+
         if postfix == "":
             postfix = self.postfix
 
